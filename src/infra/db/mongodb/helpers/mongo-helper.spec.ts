a2584af43fe1519e2d4aa948480c90ede0621f15
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-10-08 14:44:48 -0300

    refactor: fix test mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -12,6 +12,7 @@ describe('Mongo Helper', () => {
   test('Should reconnect if mongodb is down', async () => {
     let accountCollection = sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
     accountCollection = sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
   })
