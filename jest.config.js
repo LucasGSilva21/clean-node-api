860c6194df9b1f202e98ac3596bf486bf6431d94
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-10-15 17:11:16 -0300

    test

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,6 @@ module.exports = {
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  testMatch: ['**/*.test.ts']
 }
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -17,7 +17,7 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on sucess', async () => {
+  test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
