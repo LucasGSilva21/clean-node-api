0e323ce17e992f38c01adb7f27ca8bc71550ae27
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-03-22 09:45:22 -0300

    refactor: move imports to a single file

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../domain/models/account'
+export * from '../../protocols/criptography/decrypter'
+export * from '../../protocols/db/account/load-account-by-token-repository'
--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,7 +1,5 @@
 import { DbLoadAccountByToken } from './db-load-account-by-token'
-import { Decrypter } from '../../protocols/criptography/decrypter'
-import { AccountModel } from '../../../domain/models/account'
-import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
+import { Decrypter, AccountModel, LoadAccountByTokenRepository } from './db-load-account-by-token-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
