d5c7f1347fab0c72beb4f4819e827b4742cb213c
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-03-23 09:17:58 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -23,7 +23,7 @@ describe('Auth Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('Should return 400 on surveys', async () => {
+    test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -35,7 +35,7 @@ describe('Auth Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
