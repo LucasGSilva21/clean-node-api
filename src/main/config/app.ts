50fc093977893f28693da99542140772e4d2695d
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-04-27 13:18:28 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express'
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
+import setupSwagger from '@/main/config/swagger'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
   return app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,17 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: '4Dev - Enquetes para Programadores',
+    description: 'Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    version: '1.0.0',
+    contact: {
+      name: 'Lucas Gabriel Silva',
+      email: 'lucasgsilva2102@gmail.com',
+      url: 'https://www.linkedin.com/in/lucas-gabriel-30aab4183/'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
+  }
+}
