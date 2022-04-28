fa5ed7ec8fd67fda0dc989cd1869e597f3ffe949
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-04-28 09:51:02 -0300

    docs: add swagger docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -5,13 +9,31 @@ export default {
     description: 'Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
     version: '1.0.0',
     contact: {
-      name: 'Lucas Gabriel Silva',
-      email: 'lucasgsilva2102@gmail.com',
-      url: 'https://www.linkedin.com/in/lucas-gabriel-30aab4183/'
+      name: 'Rodrigo Manguinho',
+      email: 'rodrigo.manguinho@gmail.com',
+      url: 'https://www.linkedin.com/in/rmanguinho'
     },
     license: {
       name: 'GPL-3.0-or-later',
       url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
     }
-  }
+  },
+  externalDocs: {
+    description: 'Link para o treinamento completo',
+    url: 'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1'
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }, {
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
+  }],
+  paths,
+  schemas,
+  components
 }
